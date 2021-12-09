287774aa2e668a11b24338c9381a768140062865
saritazavala <saritazg2010@gmail.com>
2021-12-08 23:17:04 -0600

    First operations done

--- Parcial1/Form1.Designer.vb
+++ Parcial1/Form1.Designer.vb
@@ -26,7 +26,7 @@ Partial Class Form1
         Me.RB_LavBasic = New System.Windows.Forms.RadioButton()
         Me.RB_LavCompl = New System.Windows.Forms.RadioButton()
         Me.RB_LavMotor = New System.Windows.Forms.RadioButton()
-        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.RD_LimpChz = New System.Windows.Forms.RadioButton()
         Me.RB_Pulido = New System.Windows.Forms.RadioButton()
         Me.RB_Encerado = New System.Windows.Forms.RadioButton()
         Me.RB_Exterior = New System.Windows.Forms.RadioButton()
@@ -43,11 +43,11 @@ Partial Class Form1
         Me.Btn_Salir = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.ServDisp = New System.Windows.Forms.GroupBox()
+        Me.ChBox_ServEsp = New System.Windows.Forms.CheckBox()
+        Me.ChBox_ServBasc = New System.Windows.Forms.CheckBox()
+        Me.ChBox_DCW = New System.Windows.Forms.CheckBox()
         Me.ServBasic = New System.Windows.Forms.GroupBox()
         Me.ServEsp = New System.Windows.Forms.GroupBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
         Me.GroupBox1.SuspendLayout()
         Me.ServDisp.SuspendLayout()
         Me.ServBasic.SuspendLayout()
@@ -59,21 +59,21 @@ Partial Class Form1
         Me.RB_LavExt.AutoSize = True
         Me.RB_LavExt.Location = New System.Drawing.Point(16, 30)
         Me.RB_LavExt.Name = "RB_LavExt"
-        Me.RB_LavExt.Size = New System.Drawing.Size(133, 24)
+        Me.RB_LavExt.Size = New System.Drawing.Size(133, 64)
         Me.RB_LavExt.TabIndex = 8
         Me.RB_LavExt.TabStop = True
-        Me.RB_LavExt.Text = "Lavado Exterior"
+        Me.RB_LavExt.Text = "Lavado Exterior" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " (Q110.50)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.RB_LavExt.UseVisualStyleBackColor = True
         '
         'RB_LavBasic
         '
         Me.RB_LavBasic.AutoSize = True
-        Me.RB_LavBasic.Location = New System.Drawing.Point(16, 60)
+        Me.RB_LavBasic.Location = New System.Drawing.Point(16, 80)
         Me.RB_LavBasic.Name = "RB_LavBasic"
-        Me.RB_LavBasic.Size = New System.Drawing.Size(125, 24)
+        Me.RB_LavBasic.Size = New System.Drawing.Size(125, 44)
         Me.RB_LavBasic.TabIndex = 9
         Me.RB_LavBasic.TabStop = True
-        Me.RB_LavBasic.Text = "Lavado Basico"
+        Me.RB_LavBasic.Text = "Lavado Basico" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Q125.50)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.RB_LavBasic.UseVisualStyleBackColor = True
         '
         'RB_LavCompl
@@ -81,43 +81,43 @@ Partial Class Form1
         Me.RB_LavCompl.AutoSize = True
         Me.RB_LavCompl.Location = New System.Drawing.Point(177, 30)
         Me.RB_LavCompl.Name = "RB_LavCompl"
-        Me.RB_LavCompl.Size = New System.Drawing.Size(148, 24)
+        Me.RB_LavCompl.Size = New System.Drawing.Size(148, 44)
         Me.RB_LavCompl.TabIndex = 10
         Me.RB_LavCompl.TabStop = True
-        Me.RB_LavCompl.Text = "Lavado Completo"
+        Me.RB_LavCompl.Text = "Lavado Completo" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Q185.50)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.RB_LavCompl.UseVisualStyleBackColor = True
         '
         'RB_LavMotor
         '
         Me.RB_LavMotor.AutoSize = True
-        Me.RB_LavMotor.Location = New System.Drawing.Point(177, 60)
+        Me.RB_LavMotor.Location = New System.Drawing.Point(177, 80)
         Me.RB_LavMotor.Name = "RB_LavMotor"
-        Me.RB_LavMotor.Size = New System.Drawing.Size(144, 24)
+        Me.RB_LavMotor.Size = New System.Drawing.Size(144, 44)
         Me.RB_LavMotor.TabIndex = 11
         Me.RB_LavMotor.TabStop = True
-        Me.RB_LavMotor.Text = "Lavado de Motor"
+        Me.RB_LavMotor.Text = "Lavado de Motor" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Q165.50)"
         Me.RB_LavMotor.UseVisualStyleBackColor = True
         '
-        'RadioButton8
+        'RD_LimpChz
         '
-        Me.RadioButton8.AutoSize = True
-        Me.RadioButton8.Location = New System.Drawing.Point(96, 93)
-        Me.RadioButton8.Name = "RadioButton8"
-        Me.RadioButton8.Size = New System.Drawing.Size(156, 24)
-        Me.RadioButton8.TabIndex = 12
-        Me.RadioButton8.TabStop = True
-        Me.RadioButton8.Text = "Limpieza de Chasis"
-        Me.RadioButton8.UseVisualStyleBackColor = True
+        Me.RD_LimpChz.AutoSize = True
+        Me.RD_LimpChz.Location = New System.Drawing.Point(62, 130)
+        Me.RD_LimpChz.Name = "RD_LimpChz"
+        Me.RD_LimpChz.Size = New System.Drawing.Size(216, 24)
+        Me.RD_LimpChz.TabIndex = 12
+        Me.RD_LimpChz.TabStop = True
+        Me.RD_LimpChz.Text = "Limpieza de Chasis (Q95.50)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.RD_LimpChz.UseVisualStyleBackColor = True
         '
         'RB_Pulido
         '
         Me.RB_Pulido.AutoSize = True
         Me.RB_Pulido.Location = New System.Drawing.Point(4, 28)
         Me.RB_Pulido.Name = "RB_Pulido"
-        Me.RB_Pulido.Size = New System.Drawing.Size(72, 24)
+        Me.RB_Pulido.Size = New System.Drawing.Size(132, 24)
         Me.RB_Pulido.TabIndex = 14
         Me.RB_Pulido.TabStop = True
-        Me.RB_Pulido.Text = "Pulido"
+        Me.RB_Pulido.Text = "Pulido (Q90.00)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.RB_Pulido.UseVisualStyleBackColor = True
         '
         'RB_Encerado
@@ -125,10 +125,10 @@ Partial Class Form1
         Me.RB_Encerado.AutoSize = True
         Me.RB_Encerado.Location = New System.Drawing.Point(4, 58)
         Me.RB_Encerado.Name = "RB_Encerado"
-        Me.RB_Encerado.Size = New System.Drawing.Size(92, 24)
+        Me.RB_Encerado.Size = New System.Drawing.Size(152, 24)
         Me.RB_Encerado.TabIndex = 15
         Me.RB_Encerado.TabStop = True
-        Me.RB_Encerado.Text = "Encerado"
+        Me.RB_Encerado.Text = "Encerado (Q90.00)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.RB_Encerado.UseVisualStyleBackColor = True
         '
         'RB_Exterior
@@ -136,49 +136,49 @@ Partial Class Form1
         Me.RB_Exterior.AutoSize = True
         Me.RB_Exterior.Location = New System.Drawing.Point(4, 88)
         Me.RB_Exterior.Name = "RB_Exterior"
-        Me.RB_Exterior.Size = New System.Drawing.Size(81, 24)
+        Me.RB_Exterior.Size = New System.Drawing.Size(141, 24)
         Me.RB_Exterior.TabIndex = 16
         Me.RB_Exterior.TabStop = True
-        Me.RB_Exterior.Text = "Exterior"
+        Me.RB_Exterior.Text = "Exterior (Q90.00)"
         Me.RB_Exterior.UseVisualStyleBackColor = True
         '
         'RB_TapBasic
         '
         Me.RB_TapBasic.AutoSize = True
-        Me.RB_TapBasic.Location = New System.Drawing.Point(110, 88)
+        Me.RB_TapBasic.Location = New System.Drawing.Point(162, 88)
         Me.RB_TapBasic.Name = "RB_TapBasic"
-        Me.RB_TapBasic.Size = New System.Drawing.Size(137, 24)
+        Me.RB_TapBasic.Size = New System.Drawing.Size(197, 24)
         Me.RB_TapBasic.TabIndex = 17
         Me.RB_TapBasic.TabStop = True
-        Me.RB_TapBasic.Text = "Tapizado Basico"
+        Me.RB_TapBasic.Text = "Tapizado Basico (Q85.00)"
         Me.RB_TapBasic.UseVisualStyleBackColor = True
         '
         'RB_TapFull
         '
         Me.RB_TapFull.AutoSize = True
-        Me.RB_TapFull.Location = New System.Drawing.Point(110, 28)
+        Me.RB_TapFull.Location = New System.Drawing.Point(162, 28)
         Me.RB_TapFull.Name = "RB_TapFull"
-        Me.RB_TapFull.Size = New System.Drawing.Size(117, 24)
+        Me.RB_TapFull.Size = New System.Drawing.Size(185, 24)
         Me.RB_TapFull.TabIndex = 18
         Me.RB_TapFull.TabStop = True
-        Me.RB_TapFull.Text = "Tapizado Full"
+        Me.RB_TapFull.Text = "Tapizado Full (Q105.00)"
         Me.RB_TapFull.UseVisualStyleBackColor = True
         '
         'RB_LavLlanta
         '
         Me.RB_LavLlanta.AutoSize = True
-        Me.RB_LavLlanta.Location = New System.Drawing.Point(110, 58)
+        Me.RB_LavLlanta.Location = New System.Drawing.Point(162, 58)
         Me.RB_LavLlanta.Name = "RB_LavLlanta"
-        Me.RB_LavLlanta.Size = New System.Drawing.Size(128, 24)
+        Me.RB_LavLlanta.Size = New System.Drawing.Size(191, 24)
         Me.RB_LavLlanta.TabIndex = 19
         Me.RB_LavLlanta.TabStop = True
-        Me.RB_LavLlanta.Text = "Lavado Llantas"
+        Me.RB_LavLlanta.Text = "Lavado Llantas (Q135.00"
         Me.RB_LavLlanta.UseVisualStyleBackColor = True
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(455, 178)
+        Me.Label3.Location = New System.Drawing.Point(500, 222)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(137, 20)
         Me.Label3.TabIndex = 20
@@ -187,7 +187,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(359, 209)
+        Me.Label5.Location = New System.Drawing.Point(404, 253)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(64, 20)
         Me.Label5.TabIndex = 21
@@ -196,7 +196,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(359, 252)
+        Me.Label6.Location = New System.Drawing.Point(404, 296)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(32, 20)
         Me.Label6.TabIndex = 22
@@ -204,16 +204,16 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(429, 209)
+        Me.TextBox1.Location = New System.Drawing.Point(474, 253)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(256, 27)
+        Me.TextBox1.Size = New System.Drawing.Size(222, 27)
         Me.TextBox1.TabIndex = 23
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(428, 249)
+        Me.TextBox2.Location = New System.Drawing.Point(473, 293)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(256, 27)
+        Me.TextBox2.Size = New System.Drawing.Size(222, 27)
         Me.TextBox2.TabIndex = 24
         '
         'Btn_Calcular
@@ -257,9 +257,9 @@ Partial Class Form1
         '
         'ServDisp
         '
-        Me.ServDisp.Controls.Add(Me.CheckBox3)
-        Me.ServDisp.Controls.Add(Me.CheckBox2)
-        Me.ServDisp.Controls.Add(Me.CheckBox1)
+        Me.ServDisp.Controls.Add(Me.ChBox_ServEsp)
+        Me.ServDisp.Controls.Add(Me.ChBox_ServBasc)
+        Me.ServDisp.Controls.Add(Me.ChBox_DCW)
         Me.ServDisp.Location = New System.Drawing.Point(33, 20)
         Me.ServDisp.Name = "ServDisp"
         Me.ServDisp.Size = New System.Drawing.Size(263, 144)
@@ -267,9 +267,39 @@ Partial Class Form1
         Me.ServDisp.TabStop = False
         Me.ServDisp.Text = "SERVICIOS DISPONIBLES"
         '
+        'ChBox_ServEsp
+        '
+        Me.ChBox_ServEsp.AutoSize = True
+        Me.ChBox_ServEsp.Location = New System.Drawing.Point(6, 96)
+        Me.ChBox_ServEsp.Name = "ChBox_ServEsp"
+        Me.ChBox_ServEsp.Size = New System.Drawing.Size(161, 24)
+        Me.ChBox_ServEsp.TabIndex = 9
+        Me.ChBox_ServEsp.Text = "Servicios Especiales"
+        Me.ChBox_ServEsp.UseVisualStyleBackColor = True
+        '
+        'ChBox_ServBasc
+        '
+        Me.ChBox_ServBasc.AutoSize = True
+        Me.ChBox_ServBasc.Location = New System.Drawing.Point(6, 66)
+        Me.ChBox_ServBasc.Name = "ChBox_ServBasc"
+        Me.ChBox_ServBasc.Size = New System.Drawing.Size(142, 24)
+        Me.ChBox_ServBasc.TabIndex = 8
+        Me.ChBox_ServBasc.Text = "Servicios Basicos"
+        Me.ChBox_ServBasc.UseVisualStyleBackColor = True
+        '
+        'ChBox_DCW
+        '
+        Me.ChBox_DCW.AutoSize = True
+        Me.ChBox_DCW.Location = New System.Drawing.Point(6, 36)
+        Me.ChBox_DCW.Name = "ChBox_DCW"
+        Me.ChBox_DCW.Size = New System.Drawing.Size(199, 24)
+        Me.ChBox_DCW.TabIndex = 7
+        Me.ChBox_DCW.Text = "Delivery Car Wash (Q100)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.ChBox_DCW.UseVisualStyleBackColor = True
+        '
         'ServBasic
         '
-        Me.ServBasic.Controls.Add(Me.RadioButton8)
+        Me.ServBasic.Controls.Add(Me.RD_LimpChz)
         Me.ServBasic.Controls.Add(Me.RB_LavMotor)
         Me.ServBasic.Controls.Add(Me.RB_LavBasic)
         Me.ServBasic.Controls.Add(Me.RB_LavExt)
@@ -277,7 +307,7 @@ Partial Class Form1
         Me.ServBasic.Enabled = False
         Me.ServBasic.Location = New System.Drawing.Point(359, 20)
         Me.ServBasic.Name = "ServBasic"
-        Me.ServBasic.Size = New System.Drawing.Size(331, 138)
+        Me.ServBasic.Size = New System.Drawing.Size(355, 168)
         Me.ServBasic.TabIndex = 31
         Me.ServBasic.TabStop = False
         Me.ServBasic.Text = "SERVICIOS BASICOS"
@@ -291,42 +321,12 @@ Partial Class Form1
         Me.ServEsp.Controls.Add(Me.RB_Encerado)
         Me.ServEsp.Controls.Add(Me.RB_Pulido)
         Me.ServEsp.Enabled = False
-        Me.ServEsp.Location = New System.Drawing.Point(18, 178)
+        Me.ServEsp.Location = New System.Drawing.Point(12, 194)
         Me.ServEsp.Name = "ServEsp"
-        Me.ServEsp.Size = New System.Drawing.Size(308, 126)
+        Me.ServEsp.Size = New System.Drawing.Size(365, 154)
         Me.ServEsp.TabIndex = 32
         Me.ServEsp.TabStop = False
         Me.ServEsp.Text = "SERVICIOS ESPECIALES"
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(6, 36)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(199, 24)
-        Me.CheckBox1.TabIndex = 7
-        Me.CheckBox1.Text = "Delivery Car Wash (Q100)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(6, 66)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(142, 24)
-        Me.CheckBox2.TabIndex = 8
-        Me.CheckBox2.Text = "Servicios Basicos"
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'CheckBox3
-        '
-        Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Location = New System.Drawing.Point(6, 96)
-        Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(161, 24)
-        Me.CheckBox3.TabIndex = 9
-        Me.CheckBox3.Text = "Servicios Especiales"
-        Me.CheckBox3.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -359,7 +359,7 @@ Partial Class Form1
     Friend WithEvents RB_LavBasic As RadioButton
     Friend WithEvents RB_LavCompl As RadioButton
     Friend WithEvents RB_LavMotor As RadioButton
-    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RD_LimpChz As RadioButton
     Friend WithEvents RB_Pulido As RadioButton
     Friend WithEvents RB_Encerado As RadioButton
     Friend WithEvents RB_Exterior As RadioButton
@@ -378,7 +378,7 @@ Partial Class Form1
     Friend WithEvents ServDisp As GroupBox
     Friend WithEvents ServBasic As GroupBox
     Friend WithEvents ServEsp As GroupBox
-    Friend WithEvents CheckBox3 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ChBox_ServEsp As CheckBox
+    Friend WithEvents ChBox_ServBasc As CheckBox
+    Friend WithEvents ChBox_DCW As CheckBox
 End Class
--- Parcial1/Form1.vb
+++ Parcial1/Form1.vb
@@ -4,5 +4,148 @@
 'Miguel Bedoya
 
 Public Class Form1
+    'Dim nombre_variable as tipo_variable.
+    Dim contador As Double
 
+    Private Sub ChBox_ServBasc_CheckedChanged(sender As Object, e As EventArgs) Handles ChBox_ServBasc.CheckedChanged
+        If (ChBox_ServBasc.Checked
+            ) Then
+            ServBasic.Enabled = True
+            ServBasic.Focus()
+        Else
+            ServBasic.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ChBox_ServEsp_CheckedChanged(sender As Object, e As EventArgs) Handles ChBox_ServEsp.CheckedChanged
+        If (ChBox_ServEsp.Checked
+            ) Then
+            ServEsp.Enabled = True
+            ServEsp.Focus()
+        Else
+            ServEsp.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ChBox_DCW_CheckedChanged(sender As Object, e As EventArgs) Handles ChBox_DCW.CheckedChanged
+        If (ChBox_DCW.Checked) Then
+            contador += 100
+        Else
+            contador = contador - 100
+        End If
+        Debug.Print(contador)
+    End Sub
+
+    Private Sub RB_LavExt_CheckedChanged(sender As Object, e As EventArgs) Handles RB_LavExt.CheckedChanged
+        If (RB_LavExt.Checked
+            ) Then
+            contador = contador + 110.5
+        Else
+            contador = contador - 110.5
+        End If
+        Debug.Print(contador)
+    End Sub
+
+    Private Sub RB_LavCompl_CheckedChanged(sender As Object, e As EventArgs) Handles RB_LavCompl.CheckedChanged
+        If (RB_LavCompl.Checked) Then
+            contador = contador + 185.5
+        Else
+            contador = contador - 185.5
+        End If
+        Debug.Print(contador)
+    End Sub
+
+    Private Sub RB_LavBasic_CheckedChanged(sender As Object, e As EventArgs) Handles RB_LavBasic.CheckedChanged
+        If (RB_LavBasic.Checked) Then
+            contador = contador + 125.5
+        Else
+            contador = contador - 125.5
+        End If
+        Debug.Print(contador)
+    End Sub
+
+    Private Sub RB_LavMotor_CheckedChanged(sender As Object, e As EventArgs) Handles RB_LavMotor.CheckedChanged
+        If (RB_LavMotor.Checked) Then
+            contador = contador + 165.5
+        Else
+            contador = contador - 165.5
+        End If
+        Debug.Print(contador)
+    End Sub
+
+    Private Sub RB_Pulido_CheckedChanged(sender As Object, e As EventArgs) Handles RB_Pulido.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_Pulido.Checked) Then
+                contador = contador + 90
+            Else
+                contador = contador - 90
+            End If
+            Debug.Print(contador)
+        End If
+    End Sub
+
+    Private Sub RB_Encerado_CheckedChanged(sender As Object, e As EventArgs) Handles RB_Encerado.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_Encerado.Checked) Then
+                contador = contador + 90
+            Else
+                contador = contador - 90
+            End If
+            Debug.Print(contador)
+        End If
+    End Sub
+
+    Private Sub RB_Exterior_CheckedChanged(sender As Object, e As EventArgs) Handles RB_Exterior.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_Exterior.Checked) Then
+                contador = contador + 90
+            Else
+                contador = contador - 90
+            End If
+            Debug.Print(contador)
+        End If
+    End Sub
+
+    Private Sub RB_TapFull_CheckedChanged(sender As Object, e As EventArgs) Handles RB_TapFull.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_TapFull.Checked) Then
+                contador = contador + 105
+            Else
+                contador = contador - 105
+            End If
+            Debug.Print(contador)
+        End If
+
+    End Sub
+
+    Private Sub RB_LavLlanta_CheckedChanged(sender As Object, e As EventArgs) Handles RB_LavLlanta.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_LavLlanta.Checked) Then
+                contador = contador + 135
+            Else
+                contador = contador - 135
+            End If
+            Debug.Print(contador)
+        End If
+    End Sub
+
+    Private Sub RB_TapBasic_CheckedChanged(sender As Object, e As EventArgs) Handles RB_TapBasic.CheckedChanged
+        If (ServEsp.Enabled) Then
+            If (RB_TapBasic.Checked) Then
+                contador = contador + 85
+            Else
+                contador = contador - 85
+            End If
+            Debug.Print(contador)
+        End If
+    End Sub
+
+    Private Sub RD_LimpChz_CheckedChanged(sender As Object, e As EventArgs) Handles RD_LimpChz.CheckedChanged
+        If (RD_LimpChz.Checked) Then
+            contador = contador + 95.5
+        Else
+            contador = contador - 95.5
+        End If
+        Debug.Print(contador)
+    End Sub
 End Class
